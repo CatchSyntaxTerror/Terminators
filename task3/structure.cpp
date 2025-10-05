0deef5ebd3f4e55a0520e588f9829be917ffbd08
Yue Hu <yue@sahu.cs.unm.edu>
2025-10-05 14:50:44 -0600

    new

---
+++ task3/structure.cpp
@@ -0,0 +1,25 @@
+enum class TokenType {
+    // identifiers & literals
+    ID, NUM,
+    // keywords
+    IF, THEN, ELSE, FI,
+    WHILE, DO, OD,
+    SKIP, TRUE, FALSE,
+    NOT, AND, OR,
+    // symbols/operators
+    ASSIGN,     // :=
+    SEMI,       // ;
+    LPAR, RPAR, // ( )
+    LBRACK, RBRACK, // [ ]
+    PLUS, MINUS, MULTI, // + - *
+    EQ, LT, GT, LEQ, GEQ, // = < > <= >=
+    // end & invalid
+    END_OF_FILE, INVALID
+};
+
+struct Token {
+    TokenType type;
+    string lexeme;
+    long long intValue = 0;
+    int line = 1;
+};
